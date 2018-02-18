bc7a18520faf2df3353a83f580fb2bbc93bd799e
DanniGudna <dannigudna@gmail.com>
2018-02-18 23:00:23 +0000

    stillt nágranna og betra hnitakerfi

--- Lokaverkefni 2.0/Assets/Scripts/HexCell.cs
+++ Lokaverkefni 2.0/Assets/Scripts/HexCell.cs
@@ -8,4 +8,18 @@ public class HexCell : MonoBehaviour {
 
 	public Color color;
 
+	[SerializeField]
+	HexCell[] neighbors;
+
+	public HexCell GetNeighbor (HexDirection direction) {
+		return neighbors[(int)direction];
+	}
+
+
+
+	//ef cell B er nágranni cell A þá er cell A líka nágranna cell B getum still báða í einu
+	public void SetNeighbor (HexDirection direction, HexCell cell) {
+		neighbors[(int)direction] = cell;
+		cell.neighbors[(int)direction.Opposite()] = this;
+	}
 }
